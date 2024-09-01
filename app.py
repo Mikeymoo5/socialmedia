8fec72325087b3bc8eeb1de69573ed535a3dd523
Mikeymoo5 <moodids@outlook.com>
2024-08-31 21:51:29 -0500

    Fix bug

--- app.py
+++ app.py
@@ -26,11 +26,12 @@ def signup_page():
     if form.validate_on_submit():
         usr = form.usr.data
         password = form.password.data
-        cursor.execute(f'''SELECT * FROM users WHERE username={usr} AND password={password} ''')
-        if cursor.rowcount == 1:
-            return "<p>LOGGED IN</p>"
-        else:
-            return "<p>Account name or password is incorrect</p>"
+        cursor = sqlite3.connect("database.db").cursor()
+        try:
+            cursor.execute(f'''SELECT username FROM users WHERE username={usr} AND password={password} ''')
+        except: 
+            return "<p>Account already exists</p>"
+        cursor.execute(f'''INSERT INTO users VALUES ({usr}, {password}))''')
     return render_template("form.html", page_title="Signup", form=form)
 
 @app.route("/login", methods=["GET", "POST"])
@@ -40,12 +41,11 @@ def login_page():
         usr = form.usr.data
         password = form.password.data
         cursor = sqlite3.connect("database.db").cursor()
-        cursor.execute(f'''SELECT username FROM users WHERE username={usr} AND password={password} ''')
-        if cursor.rowcount == 1:
-            return "<p>LOGGED IN</p>"
-        else:
+        try:
+            cursor.execute(f'''SELECT username FROM users WHERE username={usr} AND password={password} ''')
+        except: 
             return "<p>Account name or password is incorrect</p>"
-
+        return "<p>LOGGED IN</p>"
     return render_template("form.html", page_title="Login", form=form)
 
 class GenericUserForm(FlaskForm):
